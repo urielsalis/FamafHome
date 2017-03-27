1039b38a33ec91d8034bc9fa4b52929e7f704bf1
URIEL EZEQUIEL SALISCHIKER <usalischiker654@lab31.famaf.unc.edu.ar>
2017-03-27 17:49:56 -0300

    clase 2

--- IntroAlg1/clases/clase1-2/main.hs
+++ IntroAlg1/clases/clase1-2/main.hs
@@ -1,8 +1,8 @@
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
-			then x
-			else x*2
+                        then x
+                        else x*2
 
 doubleSmallNumber2 x = (if x > 100 then x else x*2)
 circumference :: Double -> Double
@@ -28,7 +28,29 @@ dispersion x y z = (max3 x y z)-(min3 x y z)
 celsiusToFahr x = x*1.8 + 32 
 fahrToCelsius x = (x-32)/1.8
 
+haceFrioF :: Float->Bool
 haceFrioF x = (fahrToCelsius x) < 8
 
-segundo3 :: (Num) -> Num
-segundo3 x = x !! 1
+segundo3 (x, y, z) = (x!!1)
+
+soloPares :: [Int] -> [Int]
+soloPares [] = []
+soloPares (x:xs) | (mod x 2)==0 = x:soloPares xs
+                 | (mod x 2)/=0 = soloPares xs
+
+mayoresQue10 :: [Int] -> [Int]
+mayoresQue10 [] = []
+mayoresQue10 (x:xs) | x>10 = x:mayoresQue10 xs
+                    | x<10 = mayoresQue10 xs
+
+
+mayoresQue :: Int -> [Int] -> [Int]
+mayoresQue n [] = []
+mayoresQue n (x:xs) | x>n = x:(mayoresQue n xs)
+                    | x<n = (mayoresQue n xs)
+
+sumar1 :: [Int] -> [Int]
+sumar1 [] = []
+sumar1 (x:xs) = (x+1):sumar1(xs)
+
+
